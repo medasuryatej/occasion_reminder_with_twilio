0efe19274ae01748f2ee2626e960416f27d64472
Surya tej <medasuryatej@gmail.com>
2022-02-25 20:38:21 -0500

    Create credentials.py
    Credentials file

---
+++ credentials.py
@@ -0,0 +1,4 @@
+account_sid = "your twilio account sid here"
+auth_token = "your twilio auth token here"
+my_cell = "your verified phone number here"  # example "+11234567890"
+my_twilio = "your twilio generated number here"
